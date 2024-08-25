4a057401f41c7c3048920d72d1f2a787c2ad755a
donkennie <ajeigbekehinde160@gmail.com>
2024-08-25 18:17:33 +0100

    implement register and login endpoint

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,33 +1,42 @@
 const asyncHandler = require('express-async-handler');
 const Task = require('../models/taskModel');
 
-const getTasks = asyncHandler(async(req, res) => {
+const getTasks = asyncHandler(async (req, res) => {
     const tasks = await Task.find();
     res.status(200).json(tasks);
 
 })
 
-const setTask = asyncHandler( async (req, res) => {
-    if(!req.body.text){
+const setTask = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400);
         throw new Error("Please enter a task");
     }
-    const task = await Task.create({text: req.body.text});
+    const task = await Task.create({ text: req.body.text });
 
     res.status(200).json(task);
 })
 
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async (req, res) => {
+    const task = await Task.findById(req.params.id)
+    if (!task) {
+        res.status(400)
+        throw new Error("Task not found")
+    }
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedTask);
+})
+    
+const deleteTask = asyncHandler(async(req, res) => {
+    const task = await Task.findById(req.params.id)
+        if (!task) {
+            res.status(400)
+            throw new Error("Task not found")
+        }
 
-    res.status(200).json({ message: `Task ${req.params.id} updated.` });
-
-}
-
-const deleteTask = (req, res) => {
-
-    res.status(200).json({ message: `Task ${req.params.id} deleted.` });
-
-}
+        await Task.findByIdAndDelete(req.params.id)
+        res.status(200).json({ id: req.params.id });
+})
 
 
-module.exports = { getTasks , setTask, updateTask, deleteTask}
+module.exports = { getTasks, setTask, updateTask, deleteTask }
--- server.js
+++ server.js
@@ -11,5 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
