a644dac3fbffb6f9272fa6422b88716789288e9c
donkennie <ajeigbekehinde160@gmail.com>
2024-09-01 03:38:08 +0100

    complete register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
+import Header from './components/Header';
+
 function App() {
   return (
     <Router>
       <div className='container'>
+        <Header/>
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/login' element={<Login />} />
