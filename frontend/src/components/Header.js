f00a399d55bbec23f505cabec6a65d62b2a8d9af
donkennie <ajeigbekehinde160@gmail.com>
2024-09-03 06:09:38 +0100

    add login and logout intergration

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,43 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 const Header = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector(state => state.auth)
+
+    const logoutFn = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
     return (
         <header className='header'>
-            <div className='logo'>
+            <div className="logo">
                 <Link to='/'>Task Creator</Link>
-            </div >
-
+            </div>
             <ul>
-                <li>
-                    <Link to='/login'>
-                        <FaSignInAlt /> Login
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/register'>
-                        <FaUser /> Register
-                    </Link>
-                </li>
+                {user ? (
+                    <li>
+                        <button className='btn' onClick={logoutFn}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/login'><FaSignInAlt /> Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/register'><FaUser /> Register</Link>
+                        </li>
+                    </>
+                )}
             </ul>
-        </header >
+        </header>
     )
 }
 
