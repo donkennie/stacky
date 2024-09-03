c28c885796bdf257c7b94cc7a30c94592843d67b
donkennie <ajeigbekehinde160@gmail.com>
2024-09-03 09:53:46 +0100

    add authorized user to access the dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import TaskForm from './TaskForm'
 
-const Dashboard = () =>{
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+const Dashboard = () => {
+  const navigate = useNavigate()
+  const { user } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (!user) navigate('/login')
+  }, [user, navigate])
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Tasks Dashboard</p>
+      </section>
+      <TaskForm />
+    </>
+  )
 }
+
 export default Dashboard
