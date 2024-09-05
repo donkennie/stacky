c2ec8e0b10877698515f39fddb18f1fec4aa67e0
donkennie <ajeigbekehinde160@gmail.com>
2024-09-05 17:22:22 +0100

    integrate the task crud API operations

--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -1,22 +1,25 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createTask } from '../features/tasks/taskSlice'
+import { useNavigate } from 'react-router-dom'
 
 const TaskForm = () => {
     const [text, setText] = useState('')
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const onSubmit = (e) => {
         e.preventDefault()
         dispatch(createTask({ text }))
         setText('')
+        navigate('/alltasks')
     }
 
     return (
         <section className='form'>
             <form onSubmit={onSubmit}>
                 <div className='form-group'>
-                    <label htmlFor='text'>Task</label>
+                    <label htmlFor='text' style={{ fontWeight: 'bolder'}}>Enter Task</label>
                     <input type='text' id='text' value={text} onChange={e => setText(e.target.value)}
                     />
                 </div>
